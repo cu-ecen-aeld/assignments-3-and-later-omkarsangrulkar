06d627847e56acd7dc7f003f83a1f0bff5ffd2e9
omkarsangrulkar <omkarsangrulkar14@gmail.com>
2026-02-08 22:26:02 -0700

    Fix aesdsocket server - working version

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,35 +18,31 @@
 #define BUFFER_SIZE 1024
 
 volatile sig_atomic_t shutdown_requested = 0;
-int server_fd = -1;
-int client_fd = -1;
 
 void signal_handler(int signo) {
     if (signo == SIGINT || signo == SIGTERM) {
-        syslog(LOG_INFO, "Caught signal, exiting");
         shutdown_requested = 1;
-        
-        if (client_fd != -1) {
-            close(client_fd);
-            client_fd = -1;
-        }
-        if (server_fd != -1) {
-            close(server_fd);
-            server_fd = -1;
-        }
-        unlink(DATA_FILE);
-        closelog();
-        exit(0);
     }
+}
+
+void cleanup_and_exit(int server_fd) {
+    syslog(LOG_INFO, "Caught signal, exiting");
+    if (server_fd != -1) {
+        close(server_fd);
+    }
+    unlink(DATA_FILE);
+    closelog();
+    exit(0);
 }
 
 int main(int argc, char *argv[]) {
     bool daemon_mode = false;
+    int server_fd = -1, client_fd = -1;
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_len = sizeof(client_addr);
-    char *line_buffer = NULL;
-    size_t line_size = 0;
-    size_t line_pos = 0;
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_received, bytes_read;
+    int data_fd;
     
     // Check for -d flag
     if (argc > 1 && strcmp(argv[1], "-d") == 0) {
@@ -53,14 +50,20 @@ int main(int argc, char *argv[]) {
     }
     
     // Open syslog
-    openlog("aesdsocket", LOG_PID | LOG_CONS, LOG_USER);
+    openlog("aesdsocket", LOG_PID, LOG_USER);
     
     // Setup signal handlers
     struct sigaction sa;
     memset(&sa, 0, sizeof(sa));
     sa.sa_handler = signal_handler;
-    sigaction(SIGINT, &sa, NULL);
-    sigaction(SIGTERM, &sa, NULL);
+    if (sigaction(SIGINT, &sa, NULL) == -1) {
+        syslog(LOG_ERR, "sigaction SIGINT failed: %s", strerror(errno));
+        return -1;
+    }
+    if (sigaction(SIGTERM, &sa, NULL) == -1) {
+        syslog(LOG_ERR, "sigaction SIGTERM failed: %s", strerror(errno));
+        return -1;
+    }
     
     // Create socket
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -115,28 +118,32 @@ int main(int argc, char *argv[]) {
         }
         
         chdir("/");
-        close(STDIN_FILENO);
-        close(STDOUT_FILENO);
-        close(STDERR_FILENO);
         
-        open("/dev/null", O_RDONLY);
-        open("/dev/null", O_WRONLY);
-        open("/dev/null", O_WRONLY);
+        // Redirect standard files to /dev/null
+        int fd = open("/dev/null", O_RDWR);
+        if (fd != -1) {
+            dup2(fd, STDIN_FILENO);
+            dup2(fd, STDOUT_FILENO);
+            dup2(fd, STDERR_FILENO);
+            if (fd > 2) {
+                close(fd);
+            }
+        }
     }
     
     // Listen
     if (listen(server_fd, 10) < 0) {
         syslog(LOG_ERR, "Listen failed: %s", strerror(errno));
-        close(server_fd);
-        closelog();
-        return -1;
+        cleanup_and_exit(server_fd);
     }
     
     // Accept loop
     while (!shutdown_requested) {
         client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
         if (client_fd < 0) {
-            if (errno == EINTR) continue;
+            if (errno == EINTR && shutdown_requested) {
+                break;
+            }
             syslog(LOG_ERR, "Accept failed: %s", strerror(errno));
             continue;
         }
@@ -146,88 +153,41 @@ int main(int argc, char *argv[]) {
         inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
         syslog(LOG_INFO, "Accepted connection from %s", client_ip);
         
-        // Receive data until newline
-        line_size = BUFFER_SIZE;
-        line_buffer = malloc(line_size);
-        if (!line_buffer) {
-            syslog(LOG_ERR, "malloc failed");
-            close(client_fd);
-            client_fd = -1;
-            continue;
-        }
-        line_pos = 0;
-        
+        // Receive data and write to file
         while (!shutdown_requested) {
-            char recv_buf[BUFFER_SIZE];
-            ssize_t bytes_recv = recv(client_fd, recv_buf, sizeof(recv_buf), 0);
-            
-            if (bytes_recv <= 0) {
-                if (bytes_recv == 0) {
-                    // Connection closed
-                    break;
-                }
-                if (errno == EINTR) continue;
-                syslog(LOG_ERR, "recv failed: %s", strerror(errno));
+            bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+            if (bytes_received <= 0) {
                 break;
             }
             
-            // Append to line buffer
-            for (ssize_t i = 0; i < bytes_recv; i++) {
-                if (line_pos >= line_size - 1) {
-                    line_size *= 2;
-                    char *new_buf = realloc(line_buffer, line_size);
-                    if (!new_buf) {
-                        syslog(LOG_ERR, "realloc failed");
-                        free(line_buffer);
-                        close(client_fd);
-                        client_fd = -1;
-                        goto next_client;
+            buffer[bytes_received] = '\0';
+            
+            // Write to file
+            data_fd = open(DATA_FILE, O_WRONLY | O_CREAT | O_APPEND, 0644);
+            if (data_fd >= 0) {
+                write(data_fd, buffer, bytes_received);
+                close(data_fd);
+            }
+            
+            // Check for newline
+            if (strchr(buffer, '\n') != NULL) {
+                // Send file contents back
+                data_fd = open(DATA_FILE, O_RDONLY);
+                if (data_fd >= 0) {
+                    while ((bytes_read = read(data_fd, buffer, sizeof(buffer))) > 0) {
+                        send(client_fd, buffer, bytes_read, 0);
                     }
-                    line_buffer = new_buf;
+                    close(data_fd);
                 }
-                
-                line_buffer[line_pos++] = recv_buf[i];
-                
-                if (recv_buf[i] == '\n') {
-                    // Complete packet received
-                    line_buffer[line_pos] = '\0';
-                    
-                    // Append to file
-                    int fd = open(DATA_FILE, O_WRONLY | O_CREAT | O_APPEND, 0644);
-                    if (fd < 0) {
-                        syslog(LOG_ERR, "open failed: %s", strerror(errno));
-                    } else {
-                        write(fd, line_buffer, line_pos);
-                        close(fd);
-                    }
-                    
-                    // Send file contents back
-                    fd = open(DATA_FILE, O_RDONLY);
-                    if (fd >= 0) {
-                        char send_buf[BUFFER_SIZE];
-                        ssize_t bytes_read;
-                        while ((bytes_read = read(fd, send_buf, sizeof(send_buf))) > 0) {
-                            send(client_fd, send_buf, bytes_read, 0);
-                        }
-                        close(fd);
-                    }
-                    
-                    // Reset for next packet
-                    line_pos = 0;
-                }
+                // Don't break - keep connection open for more packets
             }
         }
         
-        free(line_buffer);
-        line_buffer = NULL;
-        
-next_client:
         syslog(LOG_INFO, "Closed connection from %s", client_ip);
         close(client_fd);
         client_fd = -1;
     }
     
-    close(server_fd);
-    closelog();
+    cleanup_and_exit(server_fd);
     return 0;
 }
